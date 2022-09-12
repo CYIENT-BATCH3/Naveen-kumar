82a9b02f3179682d83b62b605936901db2423d49
Naveen172 <111974308+Naveen172@users.noreply.github.com>
2022-09-13 00:54:36 +0530

    Update even (2).c

--- week3/even (2).c
+++ week3/even (2).c
@@ -1,18 +1,18 @@
-
-#include <stdio.h>
- int main()
+/*WAp to find the given number even or not*/
+#include <stdio.h>//header file including
+ int main()//main function defination
 {
- int a;
- int i=1;
- printf("give an input:");
- scanf("%d",&a);
- while(i<=a)
+ int a;//decleration of variable
+ int i=1;//initialization
+ printf("give an input:");//asking to user toenter the input
+ scanf("%d",&a);//reading the input from the user
+ while(i<=a)//creating  loop until the conditions satisfies
  {
-     if(i%2==0)
+     if(i%2==0)//if condition is ture then it will enters into staements
      {
-         printf("%d \t",i);
+         printf("%d \t",i);//printing the integer
      }
-     i++;
+     i++;//incrementing
  }
  return 0;
 }
